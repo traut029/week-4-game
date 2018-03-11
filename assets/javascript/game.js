24aeafd8f37f2a9abd195c1396835ac59a289dcb
Eric Trautmiller <trautmillereahs@gmail.com>
2018-03-10 21:51:01 -0600

    functional, need to make pretty and make all attack values different

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,11 +31,13 @@ var playerChosenBoolean=false;
 var enemyChosenBoolean=false;
 var playerChosen="none";
 var enemyChosen="none";
+var enemiesDefeated=0;
 
 var attackArray = [lukeSkywalker.attackPower, obiWanKenobi.attackPower, darthSidious.attackPower, darthMaul.attackPower];
 var healthArray = [lukeSkywalker.health, obiWanKenobi.health, darthSidious.health, darthMaul.health];
 var counterAttackArray = [lukeSkywalker.counterAttackPower, obiWanKenobi.counterAttackPower, darthSidious.counterAttackPower, darthMaul.counterAttackPower];
 var nameArray = [lukeSkywalker.name, obiWanKenobi.name, darthSidious.name, darthMaul.name];
+var divArray = ["luke","obi","sidious","maul"]
 
 //Make sure the page loads before doing anything
 $(document).ready(function () {
@@ -43,12 +45,13 @@ $(document).ready(function () {
  
 
     //Main Process
-        console.log(attackArray);
         showHP();
+        buttonErrors();
         chooseCharacter();
         pickEnemy();
         attack();
-
+        restart();
+        winner();
 })
 
 
@@ -60,12 +63,13 @@ $(document).ready(function () {
 
 
 
-   //Show starting HP of characters
+   //Show starting HP of characters and hide restart button
 function showHP(){
     $("#lukeHP").text(lukeSkywalker.health);
     $("#obiHP").text(obiWanKenobi.health);
     $("#sidiousHP").text(darthSidious.health);
     $("#maulHP").text(darthMaul.health);
+    $("#restartButton").hide();
     }
 
 
@@ -144,24 +148,32 @@ function pickEnemy (){
         if(enemyChosenBoolean==false && playerChosenBoolean==true && playerChosen!=0 && enemyChosen=="none"){   
             $("#luke").appendTo("#defender");
             enemyChosen=0;
+            $("#attackText").text("");
+            $("#counterAttackText").text("");
         }
     })
     $("#obi").click(function () {
         if(enemyChosenBoolean==false && playerChosenBoolean==true && playerChosen!=1 && enemyChosen=="none"){     
             $("#obi").appendTo("#defender");
             enemyChosen=1;
+            $("#attackText").text("");
+            $("#counterAttackText").text("");
         }
     })
     $("#sidious").click(function () {
         if(enemyChosenBoolean==false && playerChosenBoolean==true && playerChosen!=2 && enemyChosen=="none"){ 
             $("#sidious").appendTo("#defender");
             enemyChosen=2;
+            $("#attackText").text("");
+            $("#counterAttackText").text("");
         }
     })
     $("#maul").click(function () {
         if(enemyChosenBoolean==false && playerChosenBoolean==true && playerChosen!=3 && enemyChosen=="none"){    
             $("#maul").appendTo("#defender");
             enemyChosen=3;
+            $("#attackText").text("");
+            $("#counterAttackText").text("");
         }
     })
 }
@@ -184,11 +196,45 @@ function attack(){
             $("#sidiousHP").text(healthArray[2]);
             $("#maulHP").text(healthArray[3]);
             //if you beat enemy remove them and allow for a new enemy to be picked
-            
-            //if your hp goes to zero give game over
-            console.log(healthArray[enemyChosen]);
-            console.log(healthArray[playerChosen]);
-            console.log(attackArray[playerChosen]);
+            if(healthArray[enemyChosen]<1 && enemyChosen==0) {  
+                $("#luke").remove();
+                $("#attackText").text("You have defeated " + nameArray[enemyChosen] + ", you can now choose to fight another enemy.");
+                $("#counterAttackText").text("");
+                enemyChosen="none";        
+                enemiesDefeated++;
+                console.log(enemiesDefeated);
+            }
+            if(healthArray[enemyChosen]<1 && enemyChosen==1) {  
+                $("#obi").remove();
+                $("#attackText").text("You have defeated " + nameArray[enemyChosen] + ", you can now choose to fight another enemy.");
+                $("#counterAttackText").text("");
+                enemyChosen="none";        
+                enemiesDefeated++;
+                console.log(enemiesDefeated);
+            }
+            if(healthArray[enemyChosen]<1 && enemyChosen==2) {  
+                $("#sidious").remove();
+                $("#attackText").text("You have defeated " + nameArray[enemyChosen] + ", you can now choose to fight another enemy.");
+                $("#counterAttackText").text("");
+                enemyChosen="none";        
+                enemiesDefeated++;
+                console.log(enemiesDefeated);
+            }
+            if(healthArray[enemyChosen]<1 && enemyChosen==3) {  
+                $("#maul").remove();
+                $("#attackText").text("You have defeated " + nameArray[enemyChosen] + ", you can now choose to fight another enemy.");
+                $("#counterAttackText").text("");
+                enemyChosen="none";        
+                enemiesDefeated++;
+                console.log(enemiesDefeated);
+            }
+            //if your hp goes to zero give game over and a restart button, disable attack button
+            if(healthArray[playerChosen]<1){
+            $("#attackText").text("You have been defeated... GAME OVER!!!");
+            $("#counterAttackText").text("");
+            $("#restartButton").show();
+            $("#attackButton").prop('disabled', true);
+            }
         }
     })
 }
@@ -198,6 +244,38 @@ function attack(){
 
 
 
+
+
+
+//restarts game
+function restart(){
+    $("#restartButton").click(function(){
+        location.reload();
+    })
+}
+
+
+//Displays text if a player or enemy isn't selected
+function buttonErrors(){
+    $("#attackButton").click(function(){
+        if(playerChosen=="none")
+            $("#attackText").text("Choose a character to play.");
+        if(playerChosen!="none" && enemyChosen=="none")
+            $("#attackText").text("There is no enemy here.");
+    })
+}
+
+//If all enemies are defeated display the you win text and disable attack button
+function winner(){
+    $("#attackButton").click(function(){
+        if(enemiesDefeated==3){
+            $("#attackText").text("You won!!! GAME OVER!!!");
+            $("#counterAttackText").text("");
+            $("#restartButton").show();
+            $("#attackButton").prop('disabled', true);
+        }
+    })
+}
 
 
 
